af6fb0df682750dfd3221108370371e9e9c44f62
Владимир Мельников <vladimirmelnikov@Vladimirs-MacBook-Pro.local>
2016-09-26 13:45:10 +0300

    Initial Commit

---
+++ ScrollTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScrollTest
+//
+//  Created by Владимир Мельников on 26/09/2016.
+//  Copyright © 2016 Владимир Мельников. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
